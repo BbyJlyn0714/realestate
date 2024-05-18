687598c7759fff4d9353184fb4352c61d5df0936
david <work.jdpotrido@gmail.com>
2024-05-18 23:36:51 +0800

    add frontend

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index() {
-        return view('frontend.dashboard');
+        return view('frontend.index');
     }
 }
