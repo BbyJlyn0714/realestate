a8aa5654b34f171849e61bcbfa3d5157ce853c27
david <work.jdpotrido@gmail.com>
2024-05-19 13:24:35 +0800

    Add Edit Profile

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,10 +4,45 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index() {
         return view('frontend.index');
+    }
+
+    public function profile() {
+        $user = User::find(Auth::user()->id);
+
+        return view('profile.edit',compact('user'));
+    }
+
+    public function update(Request $request) {
+        $data = User::find(Auth::user()->id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address; 
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/images/user/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName(); 
+            $file->move(public_path('upload/images/user'),$filename);
+            $data['photo'] = $filename;  
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'User Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }
 }
