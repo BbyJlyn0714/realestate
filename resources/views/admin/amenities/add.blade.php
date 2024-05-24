eab0a98d2931937799bdc1c113a312ca370652c4
david <work.jdpotrido@gmail.com>
2024-05-24 21:00:47 +0800

    add validation for amenities

--- resources/views/admin/amenities/add.blade.php
+++ resources/views/admin/amenities/add.blade.php
@@ -12,15 +12,11 @@
                 <div class="card">
                     <div class="card-body">
                         <h6 class="card-title">Add Amenities </h6>
-                        <form method="POST" action="{{ route('amenities.store') }}" class="forms-sample">
+                        <form id="myForm" method="POST" action="{{ route('amenities.store') }}" class="forms-sample">
                             @csrf
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Amenities Name </label>
-                                <input type="text" name="amenities_name"
-                                    class="form-control @error('amenitis_name') is-invalid @enderror ">
-                                @error('amenities_name')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <input type="text" name="amenities_name" class="form-control" >
                             </div>
                             <button type="submit" class="btn btn-primary me-2">Save Changes </button>
                         </form>
@@ -34,6 +30,34 @@
     </div>
 </div>
 
-
-
+<script type="text/javascript">
+    $(document).ready(function (){
+        $('#myForm').validate({
+            rules: {
+                amenities_name: {
+                    required : true,
+                },
+                
+            },
+            messages :{
+                amenities_name: {
+                    required : 'Please Enter Amenities Name',
+                }, 
+                 
+            },
+            errorElement : 'span', 
+            errorPlacement: function (error,element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight : function(element, errorClass, validClass){
+                $(element).addClass('is-invalid');
+            },
+            unhighlight : function(element, errorClass, validClass){
+                $(element).removeClass('is-invalid');
+            },
+        });
+    });
+    
+</script>
 @endsection
